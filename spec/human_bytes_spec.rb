0f6cd534bcb3c76e894ba9b9bfcb607a19c3fef8
Petr Skocik <pskocik@gmail.com>
2015-02-04 11:34:24 +0100

    expect the module to define prefixes as constants

--- spec/human_bytes_spec.rb
+++ spec/human_bytes_spec.rb
@@ -5,7 +5,19 @@ describe HumanBytes do
     expect(HumanBytes::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  context "Module constants" do
+
+    @decimal_prefixes = %i[K M G T P E Z Y]
+    @iprefixes = @decimal_prefixes.map {|p| "#{p}i".to_sym}
+    @prefixes = [:B] + @decimal_prefixes + @iprefixes
+
+    @prefixes.each do |prefix|
+      it "should define #{prefix}" do
+        expect(subject.const_defined?(prefix)).to eq(true)
+      end
+    end
+   
+    
   end
+
 end
