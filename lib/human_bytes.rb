80f120a1be6067a9fdeb406c532a6ba90b8e8a31
Petr Skocik <pskocik@gmail.com>
2015-02-04 11:34:39 +0100

    make the module define prefixes as constants

--- lib/human_bytes.rb
+++ lib/human_bytes.rb
@@ -1,5 +1,34 @@
 require "human_bytes/version"
 
 module HumanBytes
-  # Your code goes here...
+  PREFIXES =  [
+    {
+      B: 1,
+      Ki: 2**10,
+      Mi: 2**20,
+      Gi: 2**30,
+      Ti: 2**40,
+      Pi: 2**50,
+      Ei: 2**60,
+      Zi: 2**70,
+      Yi: 2**80
+    },
+    {
+      B: 1,
+      K: 10**3,
+      M: 10**6,
+      G: 10**9,
+      T: 10**12,
+      P: 10**15,
+      E: 10**18,
+      Z: 10**21,
+      Y: 10**24,
+    }
+  ]
+  B = 1
+  PREFIXES.each do |prefix_set|
+    prefix_set.each_pair do |prefix,value|
+      self.const_set(prefix,value) unless prefix == :B
+    end
+  end
 end
